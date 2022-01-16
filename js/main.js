1dbf70e8f480781a71b178a9a9382147010765ed
Yuliya <yulua03071992@gmail.com>
2022-01-16 23:04:26 +0200

    edit readme, edit sections

--- js/main.js
+++ js/main.js
@@ -8,13 +8,7 @@ function Sizing4isRight() {
 document.addEventListener('DOMContentLoaded', Sizing4isRight)
 window.addEventListener('resize', Sizing4isRight)
 
-function validate(form_id, email) {
-    var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-    var address = document.forms[form_id].elements[email].value;
-    if (reg.test(address) == false) {
-        document.getElementById('not-correct').classList.toggle('invalid-feedback');
-        document.getElementById('not-correct').classList.remove('d-none');
-        document.getElementById('subscribe').setAttribute("disabled", "disabled");
-        return false;
-    }
+function validateEmail(e) {
+    let reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    document.getElementById('subscribe').disabled = !reg.test(e.value);
 }
