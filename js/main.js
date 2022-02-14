404594c2999e807dfcdfcd2686153386bfa41025
Yuliya <yulua03071992@gmail.com>
2022-02-14 21:37:15 +0200

    do working select 'year', 'trim'

--- js/main.js
+++ js/main.js
@@ -13,50 +13,28 @@ function validateEmail(e) {
     document.getElementById('subscribe').disabled = !reg.test(e.value);
 }
 
-function filterCars(c) {
-    let x, i;
-    x = document.getElementsByClassName('card-year');
-    for (i = 0; i < x.length; i++) {
-        removeClass(x[i], '.show');
-        if (x[i].className.indexOf(c) > -1) addClass(x[i], '.show');
+
+function app() {
+    const select = document.querySelectorAll('select')
+    console.log(select)
+    const cards = document.querySelectorAll('.card')
+    console.log(cards)
+
+    function filter(category, items) {
+        items.forEach((item) => {
+            const isItemFiltered = !item.classList.contains(category);
+            console.log(isItemFiltered)
+            if (isItemFiltered) {
+                item.classList.add('hide')
+            }
+        })
     }
+    select.forEach((select) => {
+        select.addEventListener('change', (event) => {
+            const currentCategory = event.target.value
+            console.log(event.target.value)
+            filter(currentCategory, cards)
+        })
+    })
 }
-
-function addClass(element, name) {
-    let i, arr1, arr2;
-    arr1 = element.className.split(' ');
-    arr2 = name.split(' ');
-    for (i = 0; i < arr2.length; i++) {
-        if (arr1.indexOf(arr2[i]) > -1) {
-            element.className += ' ' + arr2[i];
-        }
-    }
-}
-
-function removeClass(element, name) {
-    let i, arr1, arr2;
-    arr1 = element.className.split(' ');
-    arr2 = name.split(' ');
-    for (i = 0; i < arr2.length; i++) {
-        while (arr1.indexOf(arr2[i]) > -1) {
-            arr1.splice(arr1.indexOf(arr2[i], 1))
-        }
-    };
-    element.className = arr1.join('')
-}
-
-const selectElement = document.querySelector('.year');
-
-selectElement.addEventListener('change', (event) => {
-    const result = document.querySelector('.result');
-    result.textContent = `You like ${event.target.value}`;
-    let cardYear = document.querySelector('.card-year'); // one year 2014
-    console.log(cardYear.innerHTML);
-    console.log(event.target.value)
-    let card = document.querySelectorAll('.card')
-    if (cardYear.innerHTML !== event.target.value) {
-        console.log(card)
-        for (var i = 0; i < card.length; i++) card[i].style.display = 'none';
-    }
-
-});
+app()
